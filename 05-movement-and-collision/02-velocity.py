a23afd0ae3f4d42141ad97daba03728bf61bb2d5
Humberto Rocha <humrochagf@gmail.com>
2019-09-04 08:40:25 -0300

    Add velociry

---
+++ 05-movement-and-collision/02-velocity.py
@@ -0,0 +1,42 @@
+import time
+
+import pygame
+
+BLACK = pygame.Color(0, 0, 0)
+RED = pygame.Color(255, 0, 0)
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+pygame.display.set_caption('Velocity')
+
+position_x = 0
+# 10 pyxels per second
+velocity_x = 100
+
+# capture the initial time
+ti = time.time()
+
+while True:
+    # get how much time has passed
+    # beetween the cycles
+    tf = time.time()
+    # calculate the delta
+    dt = (tf - ti)
+    # sets final time as the initial time
+    ti = tf
+
+    event = pygame.event.poll()
+
+    if event.type == pygame.QUIT:
+        break
+
+    # moves the ball one pixel per cicle
+    position_x += velocity_x * dt
+
+    screen.fill(BLACK)
+
+    pygame.draw.ellipse(screen, RED, [position_x, 300, 40, 40])
+
+    pygame.display.flip()
